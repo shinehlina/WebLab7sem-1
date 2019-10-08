fd4ccefed3a7dd5277f41eb7f3ebbf8f2af99b27
ehlinashin <ehlinashin@yandex.ru>
2019-10-09 01:39:34 +0300

    fix new bug

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  getWeatherByCity();
+});
+
 function getWeatherByCity() {
-    document.getElementById('weather').innerHTML = "";
-    document.getElementById('error').innerHTML = "";
-    let clientKey = 'ade7e1ffdaf68377d9167f51d8def411';
-    var city = document.getElementById('city').value;
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${clientKey}`)
-        .then(data => data.json())
-        .then(json => {
-            if (json.cod === 200) {
-                document.getElementById('weather').innerHTML = prepareTemplate('entry-template')(json);
-            } else {
-                document.getElementById('error').innerHTML = prepareTemplate('entry-template-err')(json);
-            }
-        })
-        .catch(e => {
-            var errorObject = {};
-            errorObject.message = e;
-            document.getElementById('error').innerHTML = prepareTemplate('entry-template-err')(json);
-        })
+  document.getElementById("weather").innerHTML = "";
+  document.getElementById("error").innerHTML = "";
+  let clientKey = "ade7e1ffdaf68377d9167f51d8def411";
+  var city = document.getElementById("city").value;
+  return fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${clientKey}`
+  )
+    .then(data => data.json())
+    .then(json => {
+      if (json.cod === 200) {
+        document.getElementById("weather").innerHTML = prepareTemplate(
+          "entry-template"
+        )(json);
+      } else {
+        document.getElementById("error").innerHTML = prepareTemplate(
+          "entry-template-err"
+        )(json);
+      }
+    })
+    .then(jj => console.log("dd"))
+    .catch(e => {
+      var errorObject = {};
+      errorObject.message = e;
+      document.getElementById("error").innerHTML = prepareTemplate(
+        "entry-template-err"
+      )(errorObject);
+    });
 }
 
 function prepareTemplate(name) {
-    return Handlebars.compile(document.getElementById(name).innerHTML);
+  return Handlebars.compile(document.getElementById(name).innerHTML);
 }
