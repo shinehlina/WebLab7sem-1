1f8f3d23baa4a72e4c8a42cea45dce1452926316
shine2 <elina.shin@emc.com>
2019-10-08 23:25:04 +0300

    done without css

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 function getWeatherByCity() {
+    document.getElementById('weather').innerHTML = "";
+    document.getElementById('error').innerHTML = "";
     let clientKey = 'ade7e1ffdaf68377d9167f51d8def411';
     var city = document.getElementById('city').value;
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${clientKey}`)
         .then(data => data.json())
         .then(json => {
-            var source = document.getElementById('entry-template').innerHTML;
-            var template = Handlebars.compile(source);
-            var weather = document.createElement('div');
-            weather.innerHTML = template(json);
-            document.body.appendChild(weather)
+            if (json.cod === 200) {
+                document.getElementById('weather').innerHTML = prepareTemplate('entry-template')(json);
+            } else {
+                document.getElementById('error').innerHTML = prepareTemplate('entry-template-err')(json);
+            }
         })
         .catch(e => {
-            console.log(e)
+            var errorObject = {};
+            errorObject.message = e;
+            document.getElementById('error').innerHTML = prepareTemplate('entry-template-err')(json);
         })
+}
+
+function prepareTemplate(name) {
+    return Handlebars.compile(document.getElementById(name).innerHTML);
 }
